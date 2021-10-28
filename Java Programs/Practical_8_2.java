c34231d7f449da9fb7ad8bc626d1b2ee46f8aee6
Yatharth Chauhan <yc232326@gmail.com>
2021-10-28 19:30:24 +0530

    Java

--- Java Programs/Practical_8_2.java
+++ Java Programs/Practical_8_2.java
@@ -2,51 +2,20 @@
 
 import java.util.*;
 
-class Student implements Comparable<Student> {
-
-    String Name;
-    int javaMarks;
-
-    Student(String name, int javaMarks) {
-
-        this.Name = name;
-        this.javaMarks = javaMarks;
-    }
-
-    public int compareTO(Student s) {
-
-        return this.javaMarks - s.javaMarks;
-    }
-
-    public String toString() {
-
-        return String.format("[%s,%d]", Name, javaMarks);
-    }
-
-    @Override
-    public int compareTo(Student o) {
-
-        return 0;
-    }
-
-}
-
 public class Practical_8_2 {
     public static void main(String[] args) {
 
-        Student[] s = new Student[4];
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(10);
+        list.add(300);
+        list.add(45);
+        list.add(2);
+        list.add(5);
 
-        s[0] = new Student("Yatharth", 98);
-        s[1] = new Student("Khushbu", 91);
-        s[2] = new Student("Deep", 89);
-        s[3] = new Student("Raj", 85);
-
-        System.out.println("Before sorting: " + Arrays.toString(s));
-        System.out.println("");
-        Arrays.sort(s);
-        System.out.println("After sorting: " + Arrays.toString(s));
+        System.out.println("BEFORE SORTING:-" + list);
+        Collections.sort(list);
+        System.out.println("AFTER SORTING:-" + list);
 
         System.out.println("\n20DCE019-Yatharth Chauhan");
-
     }
 }
